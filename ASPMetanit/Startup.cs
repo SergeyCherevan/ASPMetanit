6acaf8bd599f7c0709f6a35961f4991417702e98
SergeyCherevan <CherevanSergeyLugansk@gmail.com>
2021-02-24 00:39:02 +0200

    Добавил поле IWebHostEnvironment _env в класс Startup. Добавил к этому классу конструктор.

--- ASPMetanit/Startup.cs
+++ ASPMetanit/Startup.cs
@@ -12,6 +12,12 @@ namespace ASPMetanit
 {
     public class Startup
     {
+        IWebHostEnvironment _env;
+        public Startup(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -32,7 +38,7 @@ namespace ASPMetanit
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync($"Application Name: {_env.ApplicationName}");
                 });
             });
         }
