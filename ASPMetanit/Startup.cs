222a8b800cbd47d9ec2d78a6c4d2d8f897ae6dda
SergeyCherevan <CherevanSergeyLugansk@gmail.com>
2021-03-06 09:10:29 +0200

    На дефолт и вывести таблицу сервисов

--- ASPMetanit/Startup.cs
+++ ASPMetanit/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -12,15 +13,31 @@ namespace ASPMetanit
 {
     public class Startup
     {
+        private IServiceCollection _services;
         public void ConfigureServices(IServiceCollection services)
         {
+            _services = services;
         }
-
         public void Configure(IApplicationBuilder app)
         {
-            app.UseMiddleware<ErrorHandlingMiddleware>();
-            app.UseMiddleware<AuthenticationMiddleware>();
-            app.UseMiddleware<RoutingMiddleware>();
+            app.Run(async context =>
+            {
+                var sb = new StringBuilder();
+                sb.Append("<h1>Все сервисы</h1>");
+                sb.Append("<table>");
+                sb.Append("<tr><th>Тип</th><th>Lifetime</th><th>Реализация</th></tr>");
+                foreach (var svc in _services)
+                {
+                    sb.Append("<tr>");
+                    sb.Append($"<td>{svc.ServiceType.FullName}</td>");
+                    sb.Append($"<td>{svc.Lifetime}</td>");
+                    sb.Append($"<td>{svc.ImplementationType?.FullName}</td>");
+                    sb.Append("</tr>");
+                }
+                sb.Append("</table>");
+                context.Response.ContentType = "text/html;charset=utf-8";
+                await context.Response.WriteAsync(sb.ToString());
+            });
         }
     }
 }
